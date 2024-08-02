0d078f1cece0ac3f274d3d5310ba729b099e9d65
dburghelea <dburghelea@dburghelea.dburghelea>
2024-08-02 22:06:58 +0300

    android handling

--- app.plugin.js
+++ app.plugin.js
@@ -1,44 +1,92 @@
-const { withDangerousMod, withPlugins } = require( '@expo/config-plugins' )
-const fs = require( 'fs' )
-const path = require( 'path' )
+const { withDangerousMod, withPlugins } = require("@expo/config-plugins");
+const fs = require("fs");
+const path = require("path");
 
-async function readFile( path ) {
-  return fs.promises.readFile( path, 'utf8' )
+async function readFile(path) {
+	return fs.promises.readFile(path, "utf8");
 }
 
-async function saveFile( path, content ) {
-  return fs.promises.writeFile( path, content, 'utf8' )
+async function saveFile(path, content) {
+	return fs.promises.writeFile(path, content, "utf8");
 }
 
-module.exports = ( config ) => withPlugins( config, [ ( config ) => {
-  return withDangerousMod( config, [
-    'ios',
-    async ( config ) => {
-      const file = path.join( config.modRequest.platformProjectRoot, 'Podfile' )
-      /*
-       * You need to remove the line before adding it.
-       * If you don't do this and you run `expo prebuild` in a dirt project
-       * your file will have the same line added twice
-       */
-	  
-	  let contents =  await readFile(file)
-	  
-	  contents = contents.replace( "pod 'secp256k1', git: 'https://github.com/status-im/secp256k1.swift.git', submodules: true", "" );
-	  contents = contents.replace( "pod 'Keycard', git: 'https://github.com/status-im/Keycard.swift.git'", "" );
-	  
-	  const addedLines = `
-  pod 'Keycard', git: 'https://github.com/status-im/Keycard.swift.git'
-  pod 'secp256k1', git: 'https://github.com/status-im/secp256k1.swift.git', submodules: true
-`
-       contents = contents.replace(
-        "post_install do |installer|",
-        `${addedLines}\n\n  post_install do |installer|`
-      )
-      /*
-       * Now re-adds the content
-       */
-      await saveFile( file, contents )
-      return config
-    }
-  ] )
-} ] )
+module.exports = (config) =>
+	withPlugins(config, [
+		(config) => {
+			return withDangerousMod(config, [
+				"ios",
+				async (config) => {
+					const file = path.join(
+						config.modRequest.platformProjectRoot,
+						"Podfile",
+					);
+					/*
+					 * You need to remove the line before adding it.
+					 * If you don't do this and you run `expo prebuild` in a dirt project
+					 * your file will have the same line added twice
+					 */
+
+					let contents = await readFile(file);
+
+					contents = contents.replace(
+						"  pod 'secp256k1', git: 'https://github.com/status-im/secp256k1.swift.git', submodules: true",
+						"",
+					);
+					contents = contents.replace(
+						"  pod 'Keycard', git: 'https://github.com/status-im/Keycard.swift.git'",
+						"",
+					);
+
+					const addedLines =
+						"  pod 'Keycard', git: 'https://github.com/status-im/Keycard.swift.git'\n  pod 'secp256k1', git: 'https://github.com/status-im/secp256k1.swift.git', submodules: true";
+
+					contents = contents.replace(
+						"post_install do |installer|",
+						`\n${addedLines}\n\n  post_install do |installer|`,
+					);
+					/*
+					 * Now re-adds the content
+					 */
+					await saveFile(file, contents);
+					return config;
+				},
+			]);
+		},
+		(config) => {
+			return withDangerousMod(config, [
+				"android",
+				async (config) => {
+					const file = path.join(
+						config.modRequest.platformProjectRoot,
+						"app",
+						"build.gradle",
+					);
+					/*
+					 * You need to remove the line before adding it.
+					 * If you don't do this and you run `expo prebuild` in a dirt project
+					 * your file will have the same line added twice
+					 */
+
+					let contents = await readFile(file);
+
+					contents = contents.replace(
+						"configuration {\n      all*.exclude module: 'bcprov-jdk15on'\n    }",
+						"",
+					);
+
+					const addedLines =
+						"configuration {\n      all*.exclude module: 'bcprov-jdk15on'\n    }";
+
+					contents = contents.replace(
+						"packagingOptions {",
+						`${addedLines}\n    packagingOptions {`,
+					);
+					/*
+					 * Now re-adds the content
+					 */
+					await saveFile(file, contents);
+					return config;
+				},
+			]);
+		},
+	]);
